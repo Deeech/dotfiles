019b8098441297c46f65bd379dd4578e1e93ef8f
Евгений Клименко <deeech88@gmail.com>
2016-12-26 22:02:47 +0300

    init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
 export DOTFILES=$HOME/Projects/git/dotfiles
 
-
+# tmux
 ln -s $DOTFILES/tmux/.tmux.conf   $HOME/.tmux.conf
+
+# vim
+ln -s $DOTFILES/vim/.vimrc        $HOME/.vimrc
+ln -s $DOTFILES/vim/.vim          $HOME/.vim
