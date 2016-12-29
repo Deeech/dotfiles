c4b524f9917c87efb9b44b17f685f1e29e9ab97b
Евгений Клименко <deeech88@gmail.com>
2016-12-29 21:55:00 +0300

    init

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 export DOTFILES=$HOME/Projects/git/dotfiles
 
+mkdir $HOME/Projects/python
+mkdir $HOME/Projects/ruby
+mkdir $HOME/Projects/c
+mkdir $HOME/Projects/js
+mkdir $HOME/Projects/php
+
 # tmux
 ln -s $DOTFILES/tmux/.tmux.conf   $HOME/.tmux.conf
 
